93c2e8e027b6a3ec2b4ebd159bdee172931552dd
Quoc Bao <hoquocbao@protonmail.com>
2022-10-15 08:09:40 +0700

    add: state task form

--- src/Modules/Board/Components/DialogTask/DialogTaskForm.jsx
+++ src/Modules/Board/Components/DialogTask/DialogTaskForm.jsx
@@ -11,24 +11,20 @@ import {
   colors,
   ListItemIcon,
   ListItemText,
-  Menu,
-  OutlinedInput,
 } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { useState } from 'react';
 import { useRequest } from '../../../../app/hooks/request/useRequest';
 import LexicalEditor from '../../../../UI/Modules/LexicalEditor';
 import taskAPIs from '../../../../app/apis/taskAPIs/taskAPIs';
+import usersAPIs from '../../../../app/apis/userAPIs/usersAPIs';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBarsProgress,
   faCircleExclamation,
-  faExclamation,
-  faExclamationCircle,
-  faMarker,
   faPen,
 } from '@fortawesome/free-solid-svg-icons';
-import { faBookmark, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
 const Label = styled(InputLabel)(({ theme }) => ({
   fontSize: '12px',
@@ -67,10 +63,10 @@ const StyledAutoCompleteInput = styled(TextField)(({ theme }) => ({
     position: 'relative',
     backgroundColor: colors.blueGrey[50],
     fontSize: 13,
-    padding: '12px 0  ',
+    padding: '12px 0',
   },
   '& .MuiAutocomplete-tag': {
-    margin: '0 2px',
+    margin: '2px',
   },
 }));
 
@@ -111,57 +107,60 @@ const newOpts = (items) =>
         : colors.green[500],
   }));
 
-// const StatusList = ({ items }) => {
-//   const newOpts = items?.map((item) => ({
-//     key: item.statusId,
-//     statusName: item.statusName,
-//     icon:
-//       item.statusId === '1'
-//         ? faPen
-//         : item.statusId === '2'
-//         ? faCircleExclamation
-//         : item.statusId === '3'
-//         ? faBarsProgress
-//         : faCheckSquare,
-
-//     color:
-//       item.statusId === '1'
-//         ? colors.red[500]
-//         : item.statusId === '2'
-//         ? colors.cyan[500]
-//         : item.statusId === '3'
-//         ? colors.blue[500]
-//         : colors.green[500],
-//   }));
-//   return (
-//     <>
-//       {newOpts.map((item) => (
-
-//       ))}
-//     </>
-//   );
-// };
-
 const { getAllPriority, getAllStatus, getAllTaskType } = taskAPIs;
+const { getUsers } = usersAPIs;
 
-const DialogTaskForm = ({ projectName }) => {
+const DialogTaskForm = () => {
   const { data: allStatus } = useRequest(getAllStatus);
+  const { data: allPriority } = useRequest(getAllPriority);
+  const { data: allTaskType } = useRequest(getAllTaskType);
+  const { data: allAssignees } = useRequest(getUsers);
 
-  const [status, setStatus] = useState('1');
-  const [description, setDescription] = useState();
+  const [listUserAssign, setListUserAssign] = useState('');
+  const [taskName, setTaskName] = useState('');
+  const [statusId, setStatusId] = useState('');
+  const [priorityId, setPriorityId] = useState('');
+  const [typeId, setTypeId] = useState('');
+  const [originalEstimate, setOriginalEstimate] = useState('');
+  const [timeTrackingSpent, setTimeTrackingSpent] = useState('');
+  const [timeTrackingRemaining, setTimeTrackingRemaining] = useState('');
+  const [description, setDescription] = useState('');
 
-  const statusChangeHandler = (event) => {
-    setStatus(event.target.value);
+  const listUserAssignHandler = (e) => {
+    console.log(e);
+    setListUserAssign('');
+  };
+  const taskNameChangeHandler = (e) => {
+    setTaskName(e.target.value);
+  };
+  const statusChangeHandler = (e) => {
+    setStatusId(e.target.value);
+  };
+  const priorityChangeHandler = (e) => {
+    setPriorityId(e.target.value);
+  };
+  const typeIdChangeHandler = (e) => {
+    setTypeId(e.target.typeIdue);
+  };
+  const originalEstimateChangeHandler = (e) => {
+    setOriginalEstimate(e.target.value);
+  };
+  const timeTrackingSpentChangeHandler = (e) => {
+    setTimeTrackingSpent(e.target.value);
+  };
+  const timeTrackingRemainingChangeHandler = (e) => {
+    setTimeTrackingRemaining(e.target.value);
   };
 
   const watchEditor = (html) => {
     setDescription(html);
   };
 
-  const options = [
-    { label: 'The Godfather', id: 1 },
-    { label: 'Pulp Fiction', id: 2 },
-  ];
+  const options =
+    allAssignees?.map((item) => ({
+      label: item.name,
+      id: item.userId,
+    })) || [];
 
   return (
     <Grid2 spacing={2} container>
@@ -170,6 +169,8 @@ const DialogTaskForm = ({ projectName }) => {
           Issues Name
         </Label>
         <StyledTextInput
+          onChange={taskNameChangeHandler}
+          value={taskName}
           id='task-name'
           size='small'
           placeholder='Issues Name'
@@ -180,11 +181,18 @@ const DialogTaskForm = ({ projectName }) => {
         <Label htmlFor='issues-status' sx={{ fontSize: '12px' }}>
           Issues Status
         </Label>
-        <StyledStatusInput fullWidth id='issues-status'>
+        <StyledStatusInput
+          onChange={statusChangeHandler}
+          value={statusId}
+          fullWidth
+          id='issues-status'
+        >
+          <MenuItem disabled value=''>
+            <em>Select Status</em>
+          </MenuItem>
           {newOpts(allStatus)?.map((item) => (
             <MenuItem
               sx={{
-                fontSize: '13px',
                 width: '100%',
               }}
               value={item.key}
@@ -205,7 +213,11 @@ const DialogTaskForm = ({ projectName }) => {
                   }}
                 />
               </ListItemIcon>
-              <ListItemText sx={{ fontSize: '13px', margin: 0 }}>
+              <ListItemText
+                sx={{
+                  margin: 0,
+                }}
+              >
                 {item.statusName}
               </ListItemText>
             </MenuItem>
@@ -217,6 +229,8 @@ const DialogTaskForm = ({ projectName }) => {
           Priority
         </Label>
         <StyledSelection
+          onChange={priorityChangeHandler}
+          value={priorityId}
           sx={{ boxSizing: 'border-box' }}
           size='small'
           fullWidth
@@ -224,9 +238,11 @@ const DialogTaskForm = ({ projectName }) => {
           defaultValue=''
           placeholder='Select Assignees'
         >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {allPriority?.map((item) => (
+            <MenuItem key={item.priorityId} value={item.priorityId}>
+              {item.priority}
+            </MenuItem>
+          ))}
         </StyledSelection>
       </Grid2>
       <Grid2 xs={6}>
@@ -234,15 +250,23 @@ const DialogTaskForm = ({ projectName }) => {
           Task type
         </Label>
         <StyledSelection
-          sx={{ boxSizing: 'border-box' }}
+          onChange={typeIdChangeHandler}
+          value={typeId}
+          sx={{ boxSizing: 'border-box', textTransform: 'capitalize' }}
           size='small'
           fullWidth
           id='task-type'
           defaultValue=''
         >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {allTaskType?.map((item) => (
+            <MenuItem
+              sx={{ textTransform: 'capitalize' }}
+              key={item.id}
+              value={item.id}
+            >
+              {item.taskType}
+            </MenuItem>
+          ))}
         </StyledSelection>
       </Grid2>
       <Grid2 xs={12}>
@@ -287,7 +311,7 @@ const DialogTaskForm = ({ projectName }) => {
         <Slider size='small' defaultValue={80} valueLabelDisplay='auto' />
       </Grid2>
       <Grid2 xs={3}>
-        <Label htmlFor='o-estimate' sx={{ fontSize: '12px' }}>
+        <Label onChan htmlFor='o-estimate' sx={{ fontSize: '12px' }}>
           Original Estimate
         </Label>
         <StyledTextInput defaultValue={0} size='small' type='number' />
